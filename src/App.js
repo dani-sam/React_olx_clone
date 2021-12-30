2057a66d95172f73812b413b4484456cab3187de
dani-sam <samdani295@gmail.com>
2021-12-30 22:36:53 +0530

    complete login page  & display username

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React,{useEffect,useContext} from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 import Signup from './Pages/Signup'
+import Login from './Pages/Login'
 
 /**
  * ?  =====Import Components=====
  */
 import Home from './Pages/Home';
+import { AuthContext, FirebaseContext } from './store/firebaseContext';
 
 function App() {
+  const {setUser}=useContext(AuthContext)
+  const {firebase} =useContext(FirebaseContext)
+  useEffect(()=>{
+      firebase.auth().onAuthStateChanged((user)=>{
+        setUser(user)
+      })
+  })
   return (
     <div>
       <Router>
@@ -17,6 +26,9 @@ function App() {
         </Route>
         <Route exact path='/signup'>
           <Signup />
+        </Route>
+        <Route exact path='/login'>
+          <Login />
         </Route>
       </Router>
     </div>
--- src/store/firebaseContext.js
+++ src/store/firebaseContext.js
@@ -1,3 +1,19 @@
 import {createContext} from 'react'
+import { useState } from 'react/cjs/react.development'
 
 export const FirebaseContext = createContext(null)
+export const AuthContext=createContext(null)
+
+
+
+
+export default function Context({children}){
+    const [user,setUser]=useState(null)
+
+
+    return(
+        <AuthContext.Provider value={{user,setUser}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
